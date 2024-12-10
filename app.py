e45435cbb5819a8637ce0932e7c270186c2d4ce8
Kaushal Singh <kaushalsingh8013@gmail.com>
2024-12-11 00:30:44 +0530

    all_models_score_data_df.csv

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
 print('Ritu')
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
+from datetime import datetime
+from geopy.distance import geodesic
+import joblib
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html',fare = '$0.0')
 
-    # predict
+@app.route('/predict',methods = ('POST','GET'))
+def predict():
+    if request.method == "POST":
+        pickup_datetime = request.form['pickup_datetime']
+        pickup_longitude = float(request.form['pickup_longitude'])
+        pickup_latitude = float(request.form['pickup_latitude'])
+        dropoff_longitude = float(request.form['dropoff_longitude'])
+        dropoff_latitude = float(request.form['dropoff_latitude'])
+        passenger_count = int(request.form['passenger_count'])
+
+        pickup_datetime = datetime.strptime(pickup_datetime,'%Y-%m-%dT%H:%M')
+        
+        year = pickup_datetime.year
+        month = pickup_datetime.month
+        day = pickup_datetime.day
+        hour = pickup_datetime.hour
+        minute = pickup_datetime.minute
+        second = pickup_datetime.second
+
+        distance = geodesic((pickup_latitude,pickup_longitude),(dropoff_latitude,dropoff_longitude)).miles
+
+        X = [[pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count,distance,year,month,day,hour,minute,second]]
+
+        model = joblib.load('lr.pkl')
+
+        
+       
+        return render_template('index.html',fare =f'${round(model.predict(X)[0],2)*passenger_count}' )
+    return render_template('index.html',fare =f'$0.0' )
+
+
 
 
 if __name__ == '__main__':
