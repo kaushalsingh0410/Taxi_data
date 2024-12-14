21df9af2a477b5fbb5bd28e8a8cf1658379ff480
Kaushal Singh <kaushalsingh8013@gmail.com>
2024-12-14 19:55:48 +0530

    final model found

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask,render_template,request
 from datetime import datetime
 from geopy.distance import geodesic
 import joblib
+import pymysql as py
 app = Flask(__name__)
 
 model = joblib.load('final_model.pkl')
 scaler = joblib.load('scaler.pkl')
 
-
+conn = py.Connect(user = 'root', host = 'localhost', password = '23032001Rk@1',autocommit= True )
+cur = conn.cursor()
 
 @app.route("/")
 def index():
@@ -33,15 +35,17 @@ def predict():
         minute = pickup_datetime.minute
         second = pickup_datetime.second
 
-        distance = geodesic((pickup_latitude,pickup_longitude),(dropoff_latitude,dropoff_longitude)).miles
+        distance = round(geodesic((pickup_latitude,pickup_longitude),(dropoff_latitude,dropoff_longitude)).miles,5)
 
         X = scaler.transform([[pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count,distance,year,month,day,hour,minute,second]])
-
+        fare_amount = round(float(model.predict(X)[0]),2)
+        q = f'''insert into taxi_data.taxi values ({pickup_longitude},{pickup_latitude},{dropoff_longitude},{dropoff_latitude},{passenger_count},{distance},{year},{month},{day},{hour},{minute},{second},{fare_amount})'''
+        cur.execute(q)
         
 
         
        
-        return render_template('index.html',fare =f'${round(model.predict(X)[0],2)}' )
+        return render_template('index.html',fare =f'${fare_amount}' )
     return render_template('index.html',fare =f'$0.0' )
 
 
